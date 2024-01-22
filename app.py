4037e2b7c8ba99b06507857388c397bd12ebddce
kaptanoglu <kaptanoglu@kaptanoglus-MacBook-Pro.local>
2024-01-22 13:47:53 +0300

    change application port

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def healthz():
     return jsonify({'result': '200'})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=6000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
