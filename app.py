cba5ae593866b15b9e33d39bb572f5b1932f5253
kaptanoglu <kaptanoglu@kaptanoglus-MacBook-Pro.local>
2024-01-22 13:38:10 +0300

    new app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Kubilay Kaptanoğlu"
+@app.route('/healthz')
+def healthz():
+    return jsonify({'result': '200'})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=6000)
